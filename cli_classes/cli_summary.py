8c3abafef541a1232f81af86a03cf164ee02fb53
kkochanski <kf.kochanski@gmail.com>
2017-03-20 21:14:14 +0000

    Require 'environmentId' in 'get_summary'

--- cli_classes/cli_summary.py
+++ cli_classes/cli_summary.py
@@ -9,4 +9,4 @@ class CliSummary(CliCaller):
     def add_parser_args(self, child_parser):
         parser_argument_builder = super(CliSummary, self).add_parser_args(child_parser)
         parser_argument_builder.add_sha256_argument()
-        parser_argument_builder.add_environment_id_argument()
+        parser_argument_builder.add_environment_id_argument(True)
