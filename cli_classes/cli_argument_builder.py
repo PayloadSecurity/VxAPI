3a88f69f4be2dbc6ead492abc48a3c9ea4afa820
kkochanski <kf.kochanski@gmail.com>
2017-10-05 11:03:05 +0200

    Add support for more report file types in public result endpoint

--- cli_classes/cli_argument_builder.py
+++ cli_classes/cli_argument_builder.py
@@ -50,7 +50,7 @@ class CliArgumentBuilder:
         self.parser.add_argument('--type', '-t', type=str, choices=['bin', 'json', 'pdf', 'crt', 'maec', 'misp', 'misp-json', 'openioc', 'html', 'pcap', 'memory', 'xml'], default='xml', help='File type to return')
 
     def add_public_file_type_argument(self):
-        self.parser.add_argument('--type', '-t', type=str, choices=['bin', 'pcap'], default='bin', help='File type to return')
+        self.parser.add_argument('--type', '-t', type=str, choices=['bin', 'pcap', 'stix', 'misp', 'misp-json', 'openioc'], default='bin', help='File type to return')
 
     def add_cli_output_argument(self):
         self.parser.add_argument('--cli_output', '-o', type=str, default='output', help='Output path')
