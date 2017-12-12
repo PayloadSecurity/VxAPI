e4609d08a9f9a577fc4725785cde27c82d45d283
kkochanski <kf.kochanski@gmail.com>
2017-12-12 09:12:17 +0100

    Remove original config file and enhance config template

--- config_tpl.py
+++ config_tpl.py
@@ -1,7 +1,12 @@
+'''
+Note: this is an example configuration file (the API key does not actually work).
+Before using VxApi, please copy this file, name it 'config.py` and then put in the same directory as the current one.
+'''
+
+
 def get_config():
     return {
-        'api_key': '',
-        'api_secret': '',
-        # e.g. 'https://www.hybrid-analysis.com
-        'server': ''
+        'api_key': 'dtdqz2ewbs8os0c8cscc84cko',
+        'api_secret': 'b32444d95abcd46182e70ac1bb10e56b45bd120706198f88',
+        'server': 'https://www.hybrid-analysis.com'
     }
