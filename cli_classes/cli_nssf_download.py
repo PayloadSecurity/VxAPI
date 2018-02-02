52b2335f0b0621fffa22ec4c381515ec2025739c
kkochanski <kf.kochanski@gmail.com>
2018-02-02 19:17:28 +0100

    Sync download with the latest changes and increase auth level for nssf endpoints

--- cli_classes/cli_nssf_download.py
+++ cli_classes/cli_nssf_download.py
@@ -1,8 +1,8 @@
-from cli_classes.cli_file_saver import CliFileSaver
+from cli_classes.cli_caller import CliCaller
 
 import os
 
-class CliNssfDownload(CliFileSaver):
+class CliNssfDownload(CliCaller):
 
     help_description = 'Get NSSF samples by \'{}\''
 
@@ -14,13 +14,53 @@ class CliNssfDownload(CliFileSaver):
     def attach_args(self, args):
         super(CliNssfDownload, self).attach_args(self.convert_file_hashes_to_array(args))
 
-    def save_files(self):
-        api_response = self.api_object.api_response
-        filename = self.api_object.api_response.headers['Vx-Filename']
-        filename_without_extension, file_extension = os.path.splitext(filename)
+    def get_result_msg(self):
+        if self.api_object.api_response.headers['Content-Type'] == 'text/html':
+            raise IOError('Error has occurred. Probably too many files were attached and webservice was not able to serve them. Please check the logs there to resolve that issue.')
 
-        f_out_name = '{}/{}'.format(self.cli_output_folder, '{}_{}{}'.format(filename_without_extension, self.get_date_string(), file_extension))
-        f_out = open(f_out_name, 'wb')
-        f_out.write(api_response.content)
-        f_out.close()
+        return super(CliNssfDownload, self).get_result_msg()
+
+    def save_files(self):
+        dir = '{}/{}'.format(self.cli_output_folder, 'nssf_download_{}'.format(self.get_date_string()))
+        os.makedirs(dir)
+
+        api_response = self.api_object.api_response
+        splitted_file = api_response.content.splitlines(True)
+
+        boundary = splitted_file[0].decode('utf-8').strip()
+        lines_of_files = []
+        current_file = 0
+        processing_file = False
+        filename_identificator = 'filename="'
+        size_of_filename_identificator = len(filename_identificator)
+        for x, line in enumerate(splitted_file):
+            try:
+                string_from_line = line.decode('utf-8')  # content is mixed, we've got bytes and strings - all of them should be there
+            except UnicodeDecodeError:
+                continue
+            position_of_filename = string_from_line.find(filename_identificator)
+            if position_of_filename != -1:
+                position_of_ending = string_from_line.find('"',position_of_filename + size_of_filename_identificator + 1)
+                lines_of_files.append([string_from_line[(position_of_filename + size_of_filename_identificator):position_of_ending], None,None])
+
+            if processing_file is True:
+                if string_from_line.startswith(boundary):
+                    lines_of_files[current_file][2] = x
+
+                    if string_from_line.endswith('--'):
+                        break
+                    else:
+                        current_file = current_file + 1
+                        processing_file = False
+            elif line.isspace():
+                processing_file = True
+                lines_of_files[current_file][1] = x + 1
+                continue
+
+        for x, line_data in enumerate(lines_of_files):
+            filename = line_data[0]
+            file_bytes = splitted_file[line_data[1]:line_data[2]]
+            f_out = open('{}/{}'.format(dir, filename), 'wb')
+            f_out.writelines(file_bytes)
+            f_out.close()
 
