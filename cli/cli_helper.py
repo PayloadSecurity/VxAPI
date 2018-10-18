fb2aa425c2abcf20ec363d03ac4ba32464a3c92b
kkochanski <k.kochanski@hybrid-analysis.com>
2018-10-18 11:53:33 +0200

    Modify version check

--- cli/cli_helper.py
+++ cli/cli_helper.py
@@ -6,6 +6,6 @@ class CliHelper:
 
     @staticmethod
     def check_if_version_is_supported(args, current_version, server):
-        if args['quiet'] is False and 'hybrid-analysis.com' not in server and current_version < MINIMAL_SUPPORTED_INSTANCE_VERSION:
+        if args['quiet'] is False and 'hybrid-analysis.com' not in server and current_version.split('-')[0] < MINIMAL_SUPPORTED_INSTANCE_VERSION:
             print(Color.warning('This version of VxAPI works best on VxWebService version {} (or above). Consider upgrading to ensure the flawless performance.'.format(MINIMAL_SUPPORTED_INSTANCE_VERSION)))
 
